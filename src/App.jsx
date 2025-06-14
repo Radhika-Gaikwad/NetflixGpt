be8db87a345b1407ffc0218d7a7b7f31f65588f6
Gaikwad Radhika <radhika.sai.gaikwad@gmail.com>
2025-06-14 09:49:04 +0530

    added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react'
+function App() {
+
+
+  return (
+    <>
+      <h1 className="h-5 border-2">
+Hello
+      </h1>
+    </>
+  )
+}
+
+export default App
